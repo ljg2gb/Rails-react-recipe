ce53914bff7c67ecef273e5b0066c4ff6eda437a
Lydia Gregory <ljg2gb@virginia.edu>
2020-09-28 13:05:34 -0400

    fixes bug to successfully generate and migrate Recipes model

--- db/migrate/20200928165906_create_receipes.rb
+++ db/migrate/20200928165906_create_receipes.rb
@@ -4,7 +4,7 @@ class CreateReceipes < ActiveRecord::Migration[6.0]
       t.string :name, null: false
       t.text :ingredients, null: false
       t.text :instruction, null: false
-      t.sting :image, default: 'https://raw.githubusercontent.com/do-community/react_rails_recipe/master/app/assets/images/Sammy_Meal.jpg'
+      t.string :image, default: 'https://raw.githubusercontent.com/do-community/react_rails_recipe/master/app/assets/images/Sammy_Meal.jpg'
       t.timestamps
     end
   end
